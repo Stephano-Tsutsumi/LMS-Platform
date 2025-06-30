17289209239456e99282adf057865e470f91ef73
Stephano Tsutsumi <63266761+Stephano-Tsutsumi@users.noreply.github.com>
2025-06-30 13:59:19 -0700

    first commit

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Companion session</div>
+  )
+}
+
+export default page
+
+//This uses dynamic routing [id] wrapping the parameter into the url
