50e09bb8ad47e036b0ea6fe0afc99a37da638eb2
Stephano Tsutsumi <63266761+Stephano-Tsutsumi@users.noreply.github.com>
2025-07-02 15:22:46 -0700

    implement agent builder server actions and companion library

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -14,7 +14,33 @@ export const createCompanion = async(formData: CreateCompanion) => {
 
     if(error || !data) throw new Error(error?.message || "Failed to create Companion")
 
-    return data[0]
+    return data[0]   
+}
+
+export const getAllCompanions = async ({ limit = 10, page = 1, subject, topic }: GetAllCompanions) => {
+    const supabse = createSupabaseClient();
+
+    let query = supabse.from('companions').select()
+
+    if(subject && topic) {
+        query = query.ilike('subject', `%${subject}%`)
+        .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
+    } else if(subject) {
+        query = query.ilike('subject', `%${subject}%`)
+    } else if(topic) {
+        query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
+    }
+
+    query = query.range((page - 1) * limit, page * limit -1)
+
+    const { data: companions, error } = await query
+
+    if(error) throw new Error(error.message)
+    
+    return companions
+
+
 }
 
 //Server actions tapping into DB
+// All server actions are run on edge meaning their is not a consistant connection to the server
