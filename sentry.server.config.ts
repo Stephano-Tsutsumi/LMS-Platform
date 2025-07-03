3d4e84560b0e62ada3e0673882cb00444c257825
Stephano Tsutsumi <63266761+Stephano-Tsutsumi@users.noreply.github.com>
2025-07-03 14:52:30 -0700

    fixes session history

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://79abb5123d7f2859f56532c652ec1ca1@o4509589515599872.ingest.us.sentry.io/4509589517172736",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
